13da1578730a55ca28d995b7966090b7404305c1
David <davidmejia.05@hotmail.com>
2023-03-13 13:10:59 -0600

    prettier

--- src/components/Testimony.jsx
+++ src/components/Testimony.jsx
@@ -1,17 +1,30 @@
-import '../css/Testimony.css'
+import "../css/Testimony.css";
 
-export function Testimony({photo, name, country, position, company, testimony}){
-    return(
-        <div className="contenedor-testimonio">
-            <img className="imagen-testimonio" alt="{name}'s potho" src={require(`../images/${photo}`)}/>
+export function Testimony({
+  photo,
+  name,
+  country,
+  position,
+  company,
+  testimony,
+}) {
+  return (
+    <div className="contenedor-testimonio">
+      <img
+        className="imagen-testimonio"
+        alt={name}
+        src={require(`../images/${photo}`)}
+      />
 
-            <div className="contenedor-texto-testimonio">
-                <p className="nombre-testimonio"><strong>{name}</strong> en {country}</p>
-                <p className="cargo-testimonio">{position} en <strong>{company}</strong></p>
-                <p className="texto-testimonio">{testimony}</p>
-            </div>
-
-        </div>
-
-    );
+      <div className="contenedor-texto-testimonio">
+        <p className="nombre-testimonio">
+          <strong>{name}</strong> en {country}
+        </p>
+        <p className="cargo-testimonio">
+          {position} en <strong>{company}</strong>
+        </p>
+        <p className="texto-testimonio">{testimony}</p>
+      </div>
+    </div>
+  );
 }
