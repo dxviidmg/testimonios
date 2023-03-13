8d8eeb0574f555c0ee6df09581d9af51a85d6609
David <davidmejia.05@hotmail.com>
2023-03-13 12:25:05 -0600

    fixes

--- src/components/Testimonio.jsx
+++ src/components/Testimonio.jsx
@@ -5,11 +5,11 @@ export function Testimonio({photo, name, country, position, company, testimony})
         <div className="contenedor-testimonio">
             <img className="imagen-testimonio" alt="{name}'s potho" src={require(`../images/${photo}`)}/>
 
-        <div className="contenedor-texto-testimonio">
-            <p className="nombre-testimonio">{name} en {country}</p>
-            <p className="cargo-testimonio">{position} en {company}</p>
-            <p className="texto-testimonio">{testimony}</p>
-        </div>
+            <div className="contenedor-texto-testimonio">
+                <p className="nombre-testimonio"><strong>{name}</strong> en {country}</p>
+                <p className="cargo-testimonio">{position} en <strong>{company}</strong></p>
+                <p className="texto-testimonio">{testimony}</p>
+            </div>
 
         </div>
 
